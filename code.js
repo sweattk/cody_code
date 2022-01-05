82e393537f29b14b6c42a7e5f9356c4c4f316416
sweattk <32558898+sweattk@users.noreply.github.com>
2022-01-05 13:04:40 -0600

    my first branch switch with a commit

--- code.js
+++ code.js
@@ -1,34 +1,63 @@
 var startBtn = document.getElementById("start");
 var timerEl = document.getElementById("timer");
+var contentDiv = document.getElementById("content");
+var qDiv = document.getElementById("questions");
+
 
 var time = 100;
 var timerInterval;
 
-var gameIndex = 0;
+var gameIndex = -1;
 
 function startQuiz() {
     timerEl.textContent = time;
-    timerInterval = setInterval(Function () {
-        }, 1000);
-        
+    timerInterval = setInterval(function () {
+        time--;
+        timerEl.textContent = time;
+        if (time <= 0) {
+            endQuiz();
+        }
+    }, 1000);
 
-var questionDiv = document.createElement("div");
+    gameIndex++;
+    showQuestion();
+}
 
 function endQuiz() {
     clearInterval(timerInterval);
 }
 
-startBtn.onclick = startQuiz;
+function showQuestion() {
+    var question = questions[gameIndex];
+    qDiv.innerHTML = '';
+    var questionDiv = document.createElement("div");
+    var titleP = document.createElement("p");
+    titleP.textContent = question.title;
+    questionDiv.append(titleP);
 
-
-
-
-        time--;
-        timerEl.textContent = time;
-        if (time <=0) {
-            endQuiz();
-        }
-
-    
+    for (var i = 0; i < question.choices.length; i++) {
+        var btnEl = document.createElement("button");
+        var choice = question.choices[i];
+        btnEl.textContent = choice;
+        btnEl.onclick = checkAnswer
+        btnEl.setAttribute("data-answer", question.answer);
+        questionDiv.append(btnEl);
+    }
+    qDiv.append(questionDiv);
 }
 
+
+function checkAnswer(event) {
+    var answer = event.target.getAttribute("data-answer");
+    var choice = event.target.textContent;
+    if (choice === answer) {
+        console.log("You got it right!")
+    } else {
+        time -= 10;
+        console.log("WRONGGG")
+    }
+    gameIndex++;
+    showQuestion();
+}
+
+startBtn.onclick = startQuiz;
